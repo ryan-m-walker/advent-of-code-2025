c82f9a556779ab14c52212aaa09b3134dae8469d
ryan-m-walker <ryan.matthew.walker@gmail.com>
2025-12-01 16:35:28 -0700

    day 1, part 2

--- day_01/src/lib.rs
+++ day_01/src/lib.rs
@@ -1,11 +1,8 @@
-use std::fs;
-
-pub(crate) fn parse_input(path: &str) -> Vec<i32> {
-    let input = fs::read_to_string(path).unwrap();
-
+pub(crate) fn parse_input(input: &str) -> Vec<i32> {
     input
         .lines()
         .map(|line| {
+            let line = line.trim();
             let dir = &line[..1];
             let amount = &line[1..];
 
@@ -18,19 +15,32 @@ pub(crate) fn parse_input(path: &str) -> Vec<i32> {
         .collect()
 }
 
-pub fn part_1(input: &str) -> i32 {
+pub fn day_1(input: &str) -> (i32, i32) {
     let dirs = parse_input(input);
 
     let mut zero_count = 0;
+    let mut zero_click_count = 0;
     let mut current = 50;
 
     for dir in dirs {
-        let diff = (dir + current) % 100;
+        for _ in 0..dir.abs() {
+            if dir < 0 {
+                current -= 1;
+            } else {
+                current += 1;
+            }
 
-        if diff < 0 {
-            current = 100 + diff;
-        } else {
-            current = diff;
+            if current == 100 {
+                current = 0;
+            }
+
+            if current == -1 {
+                current = 99;
+            }
+
+            if current == 0 {
+                zero_click_count += 1;
+            }
         }
 
         if current == 0 {
@@ -38,22 +48,54 @@ pub fn part_1(input: &str) -> i32 {
         }
     }
 
-    zero_count
+    (zero_count, zero_click_count)
 }
 
 #[cfg(test)]
 mod tests {
+    use std::fs;
+
     use super::*;
 
     #[test]
     fn part_1_test_input() {
-        let res = part_1("./test_input.txt");
+        let input = fs::read_to_string("./test_input.txt").unwrap();
+        let (res, ..) = day_1(&input);
         assert_eq!(res, 3);
     }
 
     #[test]
     fn part_1_test() {
-        let res = part_1("./input.txt");
-        println!("part_1 test output = {res}");
+        let input = fs::read_to_string("./input.txt").unwrap();
+        let (res, ..) = day_1(&input);
+        println!("[part_1] = {res}");
+    }
+
+    #[test]
+    fn part_2_one_full_rotation_right_test() {
+        let input = "R60";
+        let (.., res) = day_1(input);
+        assert_eq!(res, 1);
+    }
+
+    #[test]
+    fn part_2_one_full_rotation_left_test() {
+        let input = "L60";
+        let (.., res) = day_1(input);
+        assert_eq!(res, 1);
+    }
+
+    #[test]
+    fn part_2_test_input() {
+        let input = fs::read_to_string("./test_input.txt").unwrap();
+        let (.., res) = day_1(&input);
+        assert_eq!(res, 6);
+    }
+
+    #[test]
+    fn part_2_test() {
+        let input = fs::read_to_string("./input.txt").unwrap();
+        let (.., res) = day_1(&input);
+        println!("[part_2] = {res}");
     }
 }
