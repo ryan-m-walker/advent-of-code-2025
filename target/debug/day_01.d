5c297670bce1d313b9b6c041ccd724caf500ca64
ryan-m-walker <ryan.matthew.walker@gmail.com>
2025-11-30 17:03:47 -0700

    setup

---
+++ target/debug/day_01.d
@@ -0,0 +1,1 @@
+/Users/ryan/Documents/coding/aoc/aoc_2025/target/debug/day_01: /Users/ryan/Documents/coding/aoc/aoc_2025/day_01/src/main.rs
---
+++ target/debug/deps/day_01-174f820f61e02515.d
@@ -0,0 +1,5 @@
+/Users/ryan/Documents/coding/aoc/aoc_2025/target/debug/deps/day_01-174f820f61e02515.d: day_01/src/main.rs
+
+/Users/ryan/Documents/coding/aoc/aoc_2025/target/debug/deps/day_01-174f820f61e02515: day_01/src/main.rs
+
+day_01/src/main.rs:
